736a0fb786b129deca27499a8a1a4624dd6bd2f8
Robert <robert.van.der.leeuw@gmail.com>
2026-03-12 15:33:23 +0100

    Added Makefile

---
+++ Makefile
@@ -0,0 +1,10 @@
+
+setup:
+	./iso-download.sh
+	./build-debian.sh
+
+check:
+	./iso-check.sh
+
+
+
---
+++ iso-check.sh
@@ -0,0 +1,3 @@
+# Check if ISOs/ contains: debian-custom.iso  HBCD_PE_x64.iso  memtest.iso  rescatux-0.74.iso  ubcd539.iso
+
+# After that, checksum for each (where available)
